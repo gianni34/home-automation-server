885bf3071b0fcbeef7b7b589c492ad0a9235826d
Gianni Conti <gconti34@gmail.com>
2018-09-08 12:57:35 -0300

    merge

--- HomeAutomation/business.py
+++ HomeAutomation/business.py
@@ -1,2 +1,14 @@
 # Aca tengo que refactorizar todos los metodos que tengo dispersos por clases incorrectas
 # y ponerlos aca como logica de negocio
+import time
+from HomeAutomation.models import *
+
+
+def auto_execution_scene():
+    scenes = Scene.objects.all()
+    for s in scenes:
+        if s.automatica:
+            if s.hora == time.clock():
+                Scene.execute_scene()
+
+
